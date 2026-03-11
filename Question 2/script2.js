12afc2851d1fd1a7e01875c8c3c5995fae86c872
armonyFtk <armoftk@gmail.com>
2026-03-11 22:37:31 +0200

    Question 2_Add event modified

--- Question 2/script2.js
+++ Question 2/script2.js
@@ -3,27 +3,50 @@ by: FATAKI NA
 on: 3/11/2026
 
 QUESTION 2
+Please note that the requirement in the case study says that "each word of the user input phrase should be added to the list". Which makes the output different than the output snippet given on the question paper.
 */
 
 const addButton = document.getElementById("add");
 const removeButton = document.getElementById("remove");
+const userInput = document.getElementById("item");
+const userList = document.getElementById("list");
+
+let removedCount = 0;
 
 addButton.addEventListener("click", function() {
-    let input = document.getElementById("item");
-    let userInput = input.value;
-    let userList = document.getElementById("list");
-    if (userInput !== null & userInput.trim().length !== 0) {
-        let newItem = document.createElement("li");
-        newItem.textContent = userInput;
-        userList.appendChild(newItem);
-    }
-    else {
-        alert("You didn't enter anything.")
+    
+    let text = userInput.value.trim();
+    
+    if (text.length === 0) {
+        alert("You didn't enter anything.");
+        return;
     }
 
-    input.value = "";
+    let words = text.split(" ")
+
+    for (let i=0; i < words.length; i++) {
+        if (words[i] !== "") {
+            let listitem = document.createElement("li");
+            listitem.textContent = words[i];
+            userList.appendChild(listitem);
+        }
+    }
+
+    userInput.value = "";
 });
 
 removeButton.addEventListener("click", function() {
-    
-});
+    let textLower = userInput.value.toLowerCase();
+    let items = document.getElementsByTagName("li");
+
+    for (i=0; i<items.length; i++) {
+        if (items[i].textContent.toLocaleLowerCase() = textLower) {
+
+            let removedWord = items[i].textContent;
+
+            userList.removeChild(items[i]);
+
+            
+        }
+    }
+}); 
